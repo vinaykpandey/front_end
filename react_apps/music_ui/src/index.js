07da1864c302ae7450df07d689370b689c5cd177
Vinay Pandey <vinay.pandey12@gmail.com>
2024-07-14 16:19:02 +0530

    [Vinay] Use javascript inside html tag

--- react_apps/music_ui/src/index.js
+++ react_apps/music_ui/src/index.js
@@ -5,28 +5,14 @@ import  ReactDOM from 'react-dom';
 /* jsx - javascript extension */
 //ReactDOM.render(kya dikhana hai, kaha dikhana hai)
 // React.Fragment> --> syntactic sugar --> <>
+const first_name = 'Vinay';
 ReactDOM.render(
     <>
-        <h1> Welome to PrimeVideos App picklist</h1>
-        <p>  Below are  top 5 web series from PrimeVideos </p>
-        <ol>
-            <li>
-                Family Man
-            </li>
-            <li>
-                Mirzapur
-            </li>
-            <li>
-                Hostel Days
-            </li>
-            <li>
-                Bomby diary
-            </li>
-            <li>
-                Rakshak
-            </li>
-        </ol>
-       
+        <h2> Alright, Lets start! </h2>
+        <p> Hello, Vins Striker! </p>
+        <h3> Multiple tag in wrpped tag </h3>
+        <i> My name  is first_name {first_name} </i>
+        <p> My luck number is {4+3}, {Math.random()}</p>
     </>,
     document.getElementById('root')
 );
