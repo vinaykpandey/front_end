d815d7fb70862d676d2770a76814ccc087083aad
Vinay Pandey <vinay.pandey12@gmail.com>
2024-07-15 21:58:30 +0530

    [Vinay] jsx attributes

--- react_apps/music_ui/src/index.js
+++ react_apps/music_ui/src/index.js
@@ -1,7 +1,5 @@
-/*react*/
 import React from "react";
 import  ReactDOM from 'react-dom';
-
 /* jsx - javascript extension */
 //ReactDOM.render(kya dikhana hai, kaha dikhana hai)
 // React.Fragment> --> syntactic sugar --> <>
@@ -9,19 +7,22 @@ const firstName = 'Vinay';
 const lastName = 'Pandey';
 const todayDate = new Date().toLocaleDateString();
 const currentTime =  new Date().toLocaleTimeString();
+const imgaeUrl_200 = "https://picsum.photos/200/300";
+const imgaeUrl_250 = "https://picsum.photos/250/300";
+const imgaeUrl_300 = "https://picsum.photos/300/300";
+const webLink = 'https://picsum.photos/';
 ReactDOM.render(
     <>
-        <h2> Alright, Lets start! </h2>
-        <p> Hello, Vins Striker! </p>
-        <h3> Multiple tag in wrpped tag </h3>
-        <i> My name  is  {firstName} {lastName}</i>
-        <br/>
-        <b> {firstName + " " +lastName}</b>
-        <br/>
-        <p>  {`Here come bactick template literals ${firstName} ${lastName}` }</p>
-        <p> My luck number is {4+3}, {Math.random()}</p>
+        <i>My name  is  {firstName} {lastName}</i>
         <p>Today date is: {todayDate}</p>
         <p>Current time is: {currentTime}</p>
+        <h1 contentEditable="true"> My name is {firstName}</h1>
+        <img  src={imgaeUrl_200} alt="Random Image" />
+        <img  src={imgaeUrl_250} alt="Random Image" />
+        <a href={webLink} target="_blank">
+            <img  src={imgaeUrl_300} alt="Random Image" />
+        </a>
+        
     </>,
     document.getElementById('root')
-);
+)
